e6ed88db281f4f0849e19eb5930af7d3c62885b4
jespadas <julio.espadas@gmail.com>
2021-09-21 20:23:33 +0200

    private and public routes added

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,62 @@
-import React from "react";
+import { firebase } from "../firebase/firebase-config";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
   Redirect,
 } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { JournalScreen } from "../components/journal/JournalScreen";
 import { AuthRouter } from "./AuthRouter";
+import { login } from "../actions/auth";
+import { PublicRoute } from "./PublicRoutes";
+import { PrivateRoute } from "./PrivateRoutes";
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+
+  const [checking, setChecking] = useState(true);
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+        setIsLogged(true);
+      } else {
+        setIsLogged(false);
+      }
+
+      setChecking(false);
+    });
+  }, [dispatch, setChecking, setIsLogged]);
+
+  if (checking) {
+    return (
+      <img
+        src="https://c.tenor.com/0iK9a1WkT40AAAAC/loading-white.gif"
+        style={{ display: "block", marginLeft: "auto", marginRight: "auto" }}
+        alt=""
+      />
+    );
+  }
+
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/auth" component={AuthRouter} />
+          <PublicRoute
+            isAuthenticated={isLogged}
+            path="/auth"
+            component={AuthRouter}
+          />
 
-          <Route exact path="/" component={JournalScreen} />
+          <PrivateRoute
+            isAuthenticated={isLogged}
+            exact
+            path="/"
+            component={JournalScreen}
+          />
 
           <Redirect to="/auth/login" />
 
