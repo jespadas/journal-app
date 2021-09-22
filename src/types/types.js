9938238f2732ecc99464459f55ac4de111457f2c
jespadas <julio.espadas@gmail.com>
2021-09-22 14:39:26 +0200

    loader helper and action types for notes

--- src/types/types.js
+++ src/types/types.js
@@ -6,5 +6,14 @@ export const types = {
   uiRemoveError: '[UI] remove Error',
 
   uiStartLoading: '[UI] Start loading',
-  uiFinishLoading: '[UI] Finish loading'
+  uiFinishLoading: '[UI] Finish loading',
+
+  notesNewEntry: '[Notes] New note',
+  notesActive: '[Notes] Set active note',
+  notesLoad: '[Notes] Load notes',
+  notesUpdated: '[Notes] Update note saved',
+  notesFileUrl: '[Notes] Updated image url',
+  notesDelete: '[Notes] Delete note',
+  notesLogoutCleaning: '[Notes] Logout cleaning'
+
 };
