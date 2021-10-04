e612d4910fa45524a99485a230dc60d96a91109a
jespadas <julio.espadas@gmail.com>
2021-10-04 09:07:35 +0200

    updating json

---
+++ build/precache-manifest.4f7390fbcac8f3c86fcbf3ff6eae07e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7184f047951ec4da6a86b7a4cec5f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5de7e2f6a8b643b4026a",
+    "url": "/static/css/main.f30ee08f.chunk.css"
+  },
+  {
+    "revision": "b679575fb2fa1ec02450",
+    "url": "/static/js/2.040bca2a.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/static/js/2.040bca2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de7e2f6a8b643b4026a",
+    "url": "/static/js/main.e5146cef.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
